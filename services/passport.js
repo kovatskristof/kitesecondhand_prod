4f6b34d9e58e3b0aa9584baf1a5dc51496f4ef74
kovatskristof <kovats.kristof@me.com>
2020-04-19 15:12:38 +0200

    final oauth flow

---
+++ services/passport.js
@@ -0,0 +1,62 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+        done(null, user);
+    });
+
+});
+
+passport.use(
+    new GoogleStrategy({
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+
+        }, (accessToken, refreshToken, profile, done) => {
+            User.findOne({ googleId: profile.id }).then(existingUser => {
+                if(existingUser){
+                    //find existing user profile
+                    done(null, existingUser);
+                }else{
+                    //create new user
+                    new User({googleId: profile.id})
+                        .save()
+                        .then(user => done(null, user));
+                }
+
+            });
+        }
+    )
+);
+
+passport.use(new FacebookStrategy({
+        clientID: keys.facebookAppID,
+        clientSecret: keys.facebookAppSecret,
+        callbackURL: "/auth/facebook/callback"
+    }, (accessToken, refreshToken, profile, done) => {
+        User.findOne({ facebookId: profile.id }).then(existingUser => {
+            if(existingUser){
+                //find existing user profile
+                done(null, existingUser);
+            }else{
+                //create new user
+                new User({facebookId: profile.id})
+                    .save()
+                    .then(user => done(null, user));
+            }
+
+        });
+    }
+    )
+);
