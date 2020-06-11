e0edf0518a3de10b4c8021851d2049837b82a0d8
kovatskristof <kovats.kristof@me.com>
2020-06-11 15:21:35 +0200

    Stripe, Client added to code 2

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,10 +1,7 @@
 import React from 'react';
 
 const LoginPage = () => {
-    renderContent(){
-
-    }
-
+    renderContent()
     return[
         <li key={1}><a href="/auth/google">Login with Google</a></li>,
         <li key={2}><a href="/auth/facebook">Login with Facebook</a></li>
