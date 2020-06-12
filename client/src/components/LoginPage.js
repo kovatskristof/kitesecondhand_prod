d12c8adeeb1791304f3fab8faadd3ca659f3cbf4
kovatskristof <kovats.kristof@me.com>
2020-06-12 11:59:47 +0200

    Local-singup

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import { Button } from 'reactstrap';
+import './mystyle.scss';
 
 const LoginPage = () => {
 
     return[
-        <li key={1}><a href="/auth/google">Login with Google</a></li>,
-        <li key={2}><a href="/auth/facebook">Login with Facebook</a></li>
+        <div className="centered">
+            <div className="ui middle aligned center aligned grid container">
+                <div className="column">
+                    <form action="/auth/signup" method="POST" className="ui large form">
+                        <div className="ui stacked segment">
+                            <div className="field">
+                                <div className="ui left icon input">
+                                    <i className="user icon"></i>
+                                    <input type="text" name="email" placeholder="Enter Email Address"/>
+                                </div>
+                            </div>
+                            <div className="field">
+                                <div className="ui left icon input">
+                                    <i className="lock icon"></i>
+                                    <input type="password" name="password" placeholder="Enter Password"/>
+                                </div>
+                            </div>
+                            <input type="submit" className="ui fluid large teal submit button" value="Sign Up"/>
+                            </div>
+                    </form>
+
+                </div>
+            </div>
+            <div className="button">
+                <Button variant="dark" href="/auth/google" block>Login with Google</Button>
+            </div>
+            <div className="button">
+                <Button color="secondary" size="lg" href="/auth/facebook" block>Login with Facebook</Button>
+            </div>
+        </div>
     ]
 };
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -21,6 +21,19 @@ module.exports = (app) => {
         }
     );
 
+    app.get('/auth/signup',(req,res) => {
+        res.render('signup');
+    })
+
+    app.post('/auth/signup', passport.authenticate('local-signup', {
+
+        successRedirect : '/', // redirect to the secure profile section
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }
+
+    ));
+
     app.get('/api/logout', (req, res) => {
         req.logout();
         res.redirect('/');
